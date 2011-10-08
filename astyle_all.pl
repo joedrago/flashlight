ac3dd4b2698c4ccda3ed8349c979aeec08eb55d4
Joe Drago <joedrago@gmail.com>
2011-10-08 16:15:33 +0000

    flashlight stubs
    git-svn-id: svn://drago.cc/flashlight/trunk@1 17d9603b-7e30-4805-8e58-91f734378c32

---
+++ astyle_all.pl
@@ -0,0 +1,20 @@
+use strict;
+use FindBin;
+
+my $astylerc = $FindBin::Bin . "/astylerc";
+
+my $list;
+if($^O =~ /Win32/)
+{
+    $list = join(' ', map { chomp; $_ } `dir /s /b *.c *.h`);
+    $astylerc =~ s/\//\\/g;
+}
+else
+{
+    $list = join(' ', map { chomp; $_ } `find lib game -name \\*.c`);
+    $list .= " " . join(' ', map { chomp; $_ } `find lib game -name \\*.h`);
+}
+
+my $cmd = "astyle --options=$astylerc $list";
+system($cmd);
+
